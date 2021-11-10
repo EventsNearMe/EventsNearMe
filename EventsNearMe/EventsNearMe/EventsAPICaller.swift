5104f8482f5d22e1aa0550123216d2e3e7b78fb4
wshi27 <wshi27@hotmailcom>
2021-11-09 21:08:14 -0500

    Added one call to API caller

--- EventsNearMe/EventsNearMe/EventsAPICaller.swift
+++ EventsNearMe/EventsNearMe/EventsAPICaller.swift
@@ -2,7 +2,32 @@
 //  EventsAPICaller.swift
 //  EventsNearMe
 //
-//  Created by Sarah Zheng on 11/4/21.
+//  Created by Weiwei Shi on 11/9/21.
 //
 
 import Foundation
+
+class EventsAPICaller{
+    static let client = EventsAPICaller ()
+    func getEventsByPostalCode(postalCode: Int, radius: Int, completion: @escaping ([[String:Any]]?) -> Void){
+        let p = String(postalCode)
+        let r = String(radius)
+        let url = URL(string: "https://app.ticketmaster.com/discovery/v2/events?apikey=3UJFG9ApE8TRi0TlE17F5jAQZL9q6OYS&postalCode="+p+"&radius="+r+"&unit=miles")!
+        print(url)
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringCacheData, timeoutInterval: 10)
+        
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request){ (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            }else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                
+                let embedded = dataDictionary["_embedded"] as! [String: Any]
+                let events = embedded["events"] as! [[String:Any]]
+                return completion(events)
+            }
+        }
+        task.resume()
+    }
+}
--- EventsNearMe/EventsNearMe/ViewController.swift
+++ EventsNearMe/EventsNearMe/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var events = [[String:Any]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        getEventsByPostalCode(postalCode: 11217, radius: 100)
     }
-
+    
+    func getEventsByPostalCode(postalCode: Int, radius: Int){
+        EventsAPICaller.client.getEventsByPostalCode(postalCode: postalCode, radius: radius){(events) in
+            guard let events = events else{
+                return
+            }
+            self.events = events;
+            print(events[0]["name"])
+        }
+    }
 
 }
 
