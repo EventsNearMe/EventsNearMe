331ef8ce8502a46a2369d429045b434c620b53b7
Z-Sarah <53502468+Z-Sarah@users.noreply.github.com>
2021-11-13 00:48:36 -0500

    added login feature (#30)

--- EventsNearMe/EventsNearMe/AllEventsTableViewController.swift
+++ EventsNearMe/EventsNearMe/AllEventsTableViewController.swift
@@ -11,16 +11,15 @@ import Parse
 class AllEventsTableViewController: UIViewController {
 
     @IBAction func onLogout(_ sender: Any) {
+    
         PFUser.logOut()
         
         let main = UIStoryboard(name: "Main", bundle: nil)
-        let loginViewController = main.instantiateViewController(identifier: "loginViewController")
-        
-        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-              let delegate = windowScene.delegate as? SceneDelegate else {return}
-        
-        delegate.window?.rootViewController = loginViewController
+        let loginNavController = main.instantiateViewController(identifier: "LoginNavigationController")
+
+        (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(loginNavController)
     }
+                                                                                                           
     override func viewDidLoad() {
         super.viewDidLoad()
 
