ba5b081b42f86dfd62c6fd7205e38bcc4a6d0d74
Sarah Zheng <z.sarah@outlook.com>
2021-11-13 14:24:24 -0500

    save work

---
+++ EventsNearMe/EventsNearMe/EventCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventCell.swift
+//  EventsNearMe
+//
+//  Created by Sarah Zheng on 11/13/21.
+//
+
+import UIKit
+
+class EventCell: UITableViewCell {
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
