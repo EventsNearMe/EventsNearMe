0e77ed490f592d9f88eb69ba618eb0f4d533df25
Sarah Zheng <z.sarah@outlook.com>
2021-11-15 17:24:34 -0500

    save random comments

--- EventsNearMe/EventsNearMe/AllEventsDetailsViewController.swift
+++ EventsNearMe/EventsNearMe/AllEventsDetailsViewController.swift
@@ -6,9 +6,10 @@
 //
 
 import UIKit
-import Foundation
+import Parse
+import MessageInputBar
 
-class AllEventsDetailsViewController: UIViewController {
+class AllEventsDetailsViewController: UIViewController, MessageInputBarDelegate {
 
     @IBOutlet weak var eventNameLabel: UILabel!
     @IBOutlet weak var lineUpImage1: UIImageView!
@@ -26,6 +27,17 @@ class AllEventsDetailsViewController: UIViewController {
     }
     
     var event: [String:Any]!
+    let commentBar = MessageInputBar()
+    var showsCommentBar = false
+    
+    var events = [PFObject]()
+    var selectedEvent: PFObject!
+    
+    @IBAction func commentButtom(_ sender: Any) {
+        print("Click here to display comments")
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,8 +46,82 @@ class AllEventsDetailsViewController: UIViewController {
         eventNameLabel.text = event["name"] as? String
         eventNameLabel.sizeToFit()
         
+        
+        
+        commentBar.inputTextView.placeholder = "Leave a comment..."
+        commentBar.delegate = self
+//        let center = NotificationCenter.default
+//        center.addObserver(self, selector: #selector(keyboardWillBeHidden(note:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+//        setupToolbar()
+        
     }
+    
+    override var inputAccessoryView: UIView? {
+        return commentBar
+    }
+    
+    override var canBecomeFirstResponder: Bool {
+        return true
+    }
+    
+    func messageInputBar(_ inputBar: MessageInputBar, didPressSendButtonWith text: String) {
+        // Create the comment
+        let event = PFObject(className: "Events")
+        
+        event["user"] = PFUser.current()!
+        event.saveInBackground {(success, error) in
+            if success {
+                            self.dismiss(animated: true, completion: nil)
+                            print("Event saved!")
+                        } else {
+                            print("Error saving the event!")
+                        }
+        }
+        
+        let comment = PFObject(className: "Comments")
+        comment["text"] = "commentBar.inputTextView.text"
 
+        selectedEvent = event
+        comment["event"] = selectedEvent
+        comment["author"] = PFUser.current()!
+
+        event.add(comment, forKey: "Comments")
+
+        event.saveInBackground { (success, error) in
+            if success {
+                print("Comment saved")
+            } else {
+                print("Error saving comment")
+            }
+        }
+        
+        // Clear and dismiss the input bar
+        commentBar.inputTextView.text = nil
+
+        showsCommentBar = false
+        becomeFirstResponder()
+        commentBar.inputTextView.resignFirstResponder()
+    }
+    
+//    @objc func keyboardWillBeHidden(note: Notification) {
+//        commentBar.inputTextView.text = nil
+//        showsCommentBar = false
+//        becomeFirstResponder()
+//    }
+
+//    func setupToolbar() {
+//        let bar = UIToolbar()
+//        let doneBotton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(dismissKeyboard))
+//        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+//        bar.items = [flexSpace, flexSpace, doneBotton]
+//        bar.sizeToFit()
+//        commentField.inputAccessoryView = bar
+//        commentField.inputAccessoryView = bar
+//    }
+//
+//    @objc func dismissKeyboard() {
+//        view.endEditing(true)
+//    }
     /*
     // MARK: - Navigation
 
