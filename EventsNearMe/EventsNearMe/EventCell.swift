b5be486f35b78291ae671a8f43d2c13cbeba60ff
CC <cc@lis-macbook-pro.myfiosgateway.com>
2021-11-19 10:10:10 -0500

    add ccbranch

--- EventsNearMe/EventsNearMe/EventCell.swift
+++ EventsNearMe/EventsNearMe/EventCell.swift
@@ -13,26 +13,21 @@ class EventCell: UITableViewCell {
     @IBOutlet weak var secondPosterView: UIImageView!
     @IBOutlet weak var eventLabel: UILabel!
     @IBOutlet weak var datetimeLabel: UILabel!
-    @IBOutlet weak var favButton: UIButton!
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var stateLabel: UILabel!
     
     
-    @IBAction func favoriteEvent(_ sender: Any) {
-        
-    }
+   // var favorited: Bool = false
     
-    var favorited: Bool = false
-    
-    func setFavorite(_ isFavorited: Bool) {
-        favorited = isFavorited
-        if(favorited) {
-            favButton.setImage(UIImage(named: "favor-icon-red"), for: UIControl.State.normal)
-        }else {
-            favButton.setImage(UIImage(named: "favor-icon"), for: UIControl.State.normal)
-        }
-        
-    }
+//    func setFavorite(_ isFavorited: Bool) {
+//        favorited = isFavorited
+//        if(favorited) {
+//            favButton.setImage(UIImage(named: "favor-icon-red"), for: UIControl.State.normal)
+//        }else {
+//            favButton.setImage(UIImage(named: "favor-icon"), for: UIControl.State.normal)
+//        }
+//
+//    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
